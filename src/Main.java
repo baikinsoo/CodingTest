ad336e9896010e167ad87d9bd9021d1eaaab61b9
baekinsoo <baikinsoo95@gmail.com>
2023-11-09 15:33:19 +0900

    Section 경로 탐색 ~~~ing

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,38 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-class Main {
+public class Main {
+    static int n,m, answer = 0;
+    static int[][] graph;
+    static int[] ch;
 
-    public int count(int[] arr, int capacity) {
-
-        int cnt = 1;
-        int point = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] - point >= capacity) {
-                cnt++;
-                point = arr[i];
+    public void DFS(int v) {
+        if (v == n) {
+            answer++;
+        } else {
+            for (int i = 1; i <= n; i++) {
+                if (graph[v][i] == 1 && ch[i] == 0) {
+                    ch[i] = 1;
+                    DFS(i);
+                    ch[i] = 0;
+                }
             }
         }
-        return cnt;
-    }
-
-    public int solution(int n, int m, int[] arr) {
-
-        int answer = 0;
-        Arrays.sort(arr);
-        int lt = 1;
-        int rt = arr[n - 1];
-        while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-            if (count(arr, mid) >= m) {
-                answer = mid;
-                rt = mid - 1;
-
-            } else {
-                lt = mid + 1;
-
-            }
-        }
-        return answer;
     }
 
     public static void main(String[] args) {
-
         Main main = new Main();
-        Scanner kb = new Scanner(System.in);
-
-        int a = kb.nextInt();
-        int b = kb.nextInt();
-        int[] arr = new int[a];
-
-        for (int i = 0; i < a; i++) {
-            arr[i] = kb.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        graph = new int[n + 1][n + 1];
+        ch = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            graph[x][y] = 1;
         }
-
-        System.out.println(main.solution(a, b, arr));
+        ch[1] = 1;
+        main.DFS(1);
+        System.out.println(answer);
     }
 }
